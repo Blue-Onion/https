6dc77f5839675cccfe95cc04ee966d5ceea59974
Blue-Onion <rawatadityasingh963@gmail.com>
2026-01-15 22:27:22 +0530

    chapter 1 done

--- main.go
+++ main.go
@@ -3,36 +3,47 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(out)
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				log.Fatal(err)
+				break
+			}
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out<-str
+				str = ""
+
+			}
+			str += string(data)
+		}
+		if len(str) != 0 {
+			out<-str
+		}
+	}()
+	return out
+}
 func main() {
 	f, err := os.Open("message.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	defer f.Close()
-	str:=""
-	for {
-		data:=make([]byte, 8)
-		n,err:=f.Read(data)
-		if err!=nil{
-			log.Fatal(err)
-			break
-		}
-		data=data[:n]
-		if i:=bytes.IndexByte(data,'\n');i!=-1{
-			str+=string(data[:i]);
-
-			data=data[i+1:]
-			fmt.Println(str)
-			str=""
-		}
-		str+=string(data)
-	}
-	if len(str)!=0{
-		fmt.Println(str)
+	lines:=getLinesChannel(f)
+	for line:=range lines{
+		fmt.Println(line)
 	}
 }
